089a87b34387a7286b93c3f64d36491dd46c3960
SedationH <sedationh@gmail.com>
2024-06-22 22:59:32 +0800

    chore: update

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,11 +1,36 @@
 import { Editor } from "@bytemd/react";
 import breaks from "@bytemd/plugin-breaks";
-import { useLocalStorageState } from "ahooks";
+import { useLocalStorageState, useMount } from "ahooks";
 
 const EditorView = () => {
   const [value, setValue] = useLocalStorageState<string>("editor-content", {
     defaultValue: `# Hello, World!`,
   });
+
+  const [isEditing, setIsEditing] = useLocalStorageState("isEditing", {
+    defaultValue: false,
+  });
+
+  useMount(() => {
+    // 点击预览按钮
+    setTimeout(() => {
+      const [editEle, previewEle] = document.querySelectorAll(
+        ".bytemd-toolbar-tab"
+      ) as NodeListOf<HTMLElement>;
+
+      if (!isEditing) {
+        previewEle?.click();
+      }
+
+      editEle?.addEventListener("click", () => {
+        setIsEditing(true);
+      });
+      previewEle?.addEventListener("click", () => {
+        setIsEditing(false);
+      });
+    }, 200);
+  });
+
   return (
     <Editor
       value={value}
